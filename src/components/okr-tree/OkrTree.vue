1c5148d3fae93f8b3d3fbc616bd49481af3dc8c7
qq449245884 <449245884@qq.com>
2020-10-25 15:25:35 +0800

    first commit

---
+++ src/components/okr-tree/OkrTree.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="org-chart-container">
+    <div class="org-chart-node-children">
+      <div class="org-chart-node">
+        <div class="org-chart-node-label">快乐学习有限公司</div>
+        <div class="org-chart-node-children">
+          <div class="org-chart-node">
+            <div class="org-chart-node-label">职能部</div>
+          </div>
+          <div class="org-chart-node">
+            <div class="org-chart-node-label">品牌部</div>
+            <div class="org-chart-node-children">
+              <div class="org-chart-node">
+                <div class="org-chart-node-label">UI设计</div>
+              </div>
+              <div class="org-chart-node">
+                <div class="org-chart-node-label">活动策划</div>
+              </div>
+            </div>
+          </div>
+          <div class="org-chart-node">
+            <div class="org-chart-node-label">事业部</div>
+            <div class="org-chart-node-children">
+              <div class="org-chart-node">
+                <div class="org-chart-node-label">行政</div>
+              </div>
+            </div>
+          </div>
+          <div class="org-chart-node">
+            <div class="org-chart-node-label">信息技术部</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.org-chart-container{
+  background: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 
+import Vue2OrgTree from 'vue2-org-tree'
+import 'vue2-org-tree/dist/style.css'
+
+Vue.use(Vue2OrgTree)
+Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
