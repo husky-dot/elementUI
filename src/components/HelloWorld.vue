626e88a9a759e08c8a3e4916bcd3c4a19d3abd6a
qq449245884 <449245884@qq.com>
2020-10-27 00:46:35 +0800

    1111

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
-    <Vue2OrgTree :data='tree' collapsable/>
+    <Vue2OrgTree :data='tree' collapsable horizontal
+      @on-expand="onExpand"
+      
+    />
     <hr >
     <OkrTree />
+    <hr>
+    <OkrTree2/>
   </div>
 </template>
 <script>
 import OkrTree from './okr-tree/OkrTree.vue'
+import OkrTree2 from './okr-tree/OkrTree2.vue'
 export default {
   components: {
-    OkrTree
+    OkrTree,
+    OkrTree2
   },
   data () {
     return {
           tree: {
           label: 'Owner',
           children: [{
-            label: 'Label 1'
+            label: 'Label 1',
+            children: [{
+              label: 'Label 1-1'
+            }]
           },{
             label: 'Label 2'
           },{
@@ -24,6 +34,18 @@ export default {
           }]
         }
     }
+  },
+  methods: {
+    onExpand (e, data) {
+      if ('expand' in data) {
+        data.expand = !data.expand
+        if (!data.expand && data.children) {
+          this.collapse(data.children)
+        }
+      } else {
+        this.$set(data, 'expand', true)
+      }
+    },
   }
 }
 </script>
--- src/components/okr-tree/OkrTree2.vue
+++ src/components/okr-tree/OkrTree2.vue
@@ -75,121 +75,33 @@ export default {
 .org-chart-container{
   display: block;
   width: 100%;
-  text-align: center;
 }
-
-.org-chart-node-children {
+.horizontal .org-chart-node-children{
   position: relative;
-  padding-top:20px;
+  padding-left:20px;
   transition: all 0.5s;
 }
-.org-chart-node{
-  float: left;
-  text-align: center;
-  list-style-type: none;
+.horizontal .org-chart-node{
+  display: flex;
+  align-items: center;
   position: relative;
-  padding: 20px 5px 0 5px;
+  padding: 0px 5px 0 20px;
   transition: all .5s;
 }
-/*使用 ::before 和 ::after 绘制连接器*/
-.org-chart-node::before,
-.org-chart-node::after {
-  content: '';
-  position: absolute;
-  top: 0;
-  right: 50%;
-  width: 50%;
-  border-top: 1px solid #ccc;
-  height: 20px;
+
+/*
+.horizontal.org-chart-node-children .org-chart-node{
+  display: flex;
+  align-items: center;
 }
-.org-chart-node::after{
-  right: auto;
-  left: 50%;
-  border-left: 1px solid #ccc;
-}
-/*我们需要从没有任何兄弟元素的元素中删除左右连接器*/
-.org-chart-node:only-child::after,
-.org-chart-node:only-child::before{
-  display: none;
-}
-/*从单个子节点的顶部移除空格*/
-.org-chart-node:only-child{
-  padding-top: 0;
-}
-/*从第一个子节点移除左连接器，从最后一个子节点移除右连接器*/
-.org-chart-node:first-child::before,
-.org-chart-node:last-child::after{
-  border: 0 none;
-}
-/*将垂直连接器添加回最后的节点*/
-.org-chart-node:last-child::before{
-  border-right: 1px solid #ccc;
-  border-radius: 0 5px 0 0;
-}
-.org-chart-node:first-child::after{
-  border-radius: 5px 0 0 0;
-}
-/*从父节点添加向下的连接器了*/
-.org-chart-node-children .org-chart-node-children::before{
-  content: '';
-  position: absolute;
-  top:0;
-  left: 50%;
-  border-left: 1px solid #ccc;
-  width: 0;
-  height: 20px;
-}
-.org-chart-node-label{
-  position: relative;
-  display: inline-block;
-}
-.org-chart-node-label .org-chart-node-label-inner{
+
+.horizontal.org-chart-node-children .org-chart-node .org-chart-node-label-inner{
   box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
   display: inline-block;
   padding: 10px;
   margin: 0px;
   font-size: 16px;
 }
-.org-chart-node-label:hover{
-  background: #eeeeee;
-}
-.org-chart-node-label .org-chart-node-btn{
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  width: 20px;
-  height: 20px;
-  z-index: 10;
-  margin-left: -11px;
-  margin-top: 9px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 50%;
-  box-shadow: 0 0 2px rgba(0, 0, 0, .15);
-  cursor: pointer;
-  transition: all .35s ease;
-}
-.org-chart-node-label .org-chart-node-btn:hover{
-  background-color: #e7e8e9;
-  transform: scale(1.15);
-}
-.org-chart-node-label .org-chart-node-btn::before,
-.org-chart-node-label .org-chart-node-btn::after {
-  content: '';
-  position: absolute;
-}
-.org-chart-node-label .org-chart-node-btn::before{
-  top: 50%;
-  left: 4px;
-  right: 4px;
-  height: 0;
-  border-top: 1px solid #ccc;
-}
-.org-chart-node-label .org-chart-node-btn::after{
-  top: 4px;
-  left: 50%;
-  bottom: 4px;
-  width: 0;
-  border-left: 1px solid #ccc;
-}
+ */
+
 </style>
